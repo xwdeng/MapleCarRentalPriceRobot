dc36aee2688a6ec31629b21b041ce21d166db2d5
Xiaowei Deng <xwdeng@xiaoweis-mbp.aforesolutions.com>
2017-07-18 23:02:26 -0400

    simple discountCarRentalAPI

--- discountCarRentalAPI.py
+++ discountCarRentalAPI.py
@@ -1,36 +1,57 @@
 import requests
-class DiscountCarRentalQuery:
-    def __init__(self):
-        self.header = ''
+import randomUserAgent
+import re
+from time import gmtime, strftime
+import time
+from datetime import timedelta
+import datetime
+import json
+import pprint
+
+API_URL = 'https://www.discountcar.com/WebAPI/api/v1/home/'
+HEADERS = {
+        'Authorization': 'Basic QXJ0eTEyNTpkaXNjb3VudFNlY3JldFBhc3Ml',
+        'Origin': 'https://www.discountcar.com',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
+        'Authorization': 'Basic QXJ0eTEyNTpkaXNjb3VudFNlY3JldFBhc3Ml',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Accept': 'application/json, text/plain, */*',
+        'Referer': 'https://www.discountcar.com/reservation',
+        'Connection': 'keep-alive',
+        }
+ua = randomUserAgent.UserAgent()
+
+class POST_URL:
+    def __init__(self, url, headers, data):
+        self.url = url
+        self.headers = headers
+        self.data = data
+#a = POST_URL('http://google',dict,dict)
+def GetNext30DayAvailableCarsWithRate(PlaceId):
+    today = datetime.date.today()
+    post_url_list = []
+    for i in range(1,31):
+        PickupDate = today + datetime.timedelta(days=i)
+        ReturnDate = today + datetime.timedelta(days=i+1)
+        post_url_list.append(GetAvailableCarsWithRate(
+            str(PlaceId),str(PlaceId),PickupDate, ReturnDate,"09:00", "09:00",
+            ua.random()))
+    return post_url_list
 
 
-    def setSingleDay(self, date):
-        self.date = date
-
-
-    def ReqMinimumPrice(self):
-        s=requests.session()
-        #webaddress= 'https://www.discountcar.com/Reservation.Vehicles.cfm?CarTruckReservation=1&PickupLocationID=169&ReturnLocationID=169&PickupDateTime=06%2F29%2F2015+07%3A30&ReturnDateTime=06%2F30%2F2015+07%3A30&PromotionID=&vehicleClassID=1'
-        webaddress1='https://www.discountcar.com/Reservation.Vehicles.cfm?CarTruckReservation=1&PickupLocationID=169&ReturnLocationID=169&PickupDateTime='+self.PickupDateTime+    '&ReturnDateTime='+self.ReturnDateTime +   '&PromotionID=&vehicleClassID=1'
-        #print(webaddress1)
-        try:
-            r=s.get(webaddress1);
-            str_=r.content.decode('utf-8')
-        #str_='<script>\n\nqrate = "59.99";\n\n</script>'
-            all_qrate = re.findall( r'<script>\s*qrate = "[0-9]*.[0-9]*";\s*<.script>', str_)
-        #DEBUG
-        #f = open('respond.txt','w')
-        #f.write(str_) # python will convert \n to os.linesep
-        #f.close()
-            getNum = re.compile(r'[0-9]*[.][0-9]*')
-            minPrice = 100
-            for item in all_qrate:
-                p=getNum.findall(item)
-                tmp=float(p[0])
-        #   print(tmp+'\n')
-                if tmp < minPrice:
-                    minPrice = tmp
-            return minPrice
-        except Exception as inst:
-            print(inst)
-            return -1
+def GetAvailableCarsWithRate(LocationId, ReturnLocationId, puDate,rDate,pickupTime, dropOffTime, userAgent):
+    url = API_URL + 'GetAvailableCarsWithRate'
+    headers = HEADERS
+    headers['User-Agent'] = userAgent
+    data = {
+      "LocationId": LocationId,
+      "ReturnLocationId": str(ReturnLocationId),
+      "puDate": puDate.strftime("20%y/%m/%d"),
+      "rDate": rDate.strftime("20%y/%m/%d"),
+      "pickupTime": pickupTime,
+      "dropOffTime": dropOffTime,
+      "PromotionId": "",
+      "LangId": 1
+    }
+    return POST_URL(url, headers, data)
